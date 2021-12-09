64ef9c939ef16a60becb670e6ca2b8f14864e88d
BRB3D <samuraix221@hotmail.com>
2021-12-08 23:00:21 -0500

    Props & state in the appointment implemented in form, also displaying appointments in storybook has been implemented

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 
 
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
   const text = (time) => {
     if (time) {
       return `Appointment at ${time}`
@@ -17,7 +20,8 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <>{text(time)}</>
-
+      <Header />
+      {interview ? <Show interviewer={interview.interviewer} student={interview.student} /> : <Empty />}
     </article>
 
   )
