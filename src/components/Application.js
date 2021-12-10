5dbc12e66f468ba50ab902e7c447564e275de3a3
BRB3D <samuraix221@hotmail.com>
2021-12-09 21:17:59 -0500

    Package.json proxy had the wrong number and it has been corrected, the schedule now updates onc ewhen rendered and it gets the data from axios request to scheduler api

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,23 +7,6 @@ import Appointment from "./Appointment";
 
 
 //------------------------------------MOCK DATA--------------------------------------//
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -68,6 +51,11 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days').then(resp => setDays([...resp.data]))
+
+  }, [])
 
   return (
     <main className="layout">
