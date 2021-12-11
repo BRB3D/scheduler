8565ca44f9252071262ba6be551ad1a730b1d1ef
BRB3D <samuraix221@hotmail.com>
2021-12-10 23:05:39 -0500

    data from api is entering and displaying in the setDays

--- src/components/Application.js
+++ src/components/Application.js
@@ -53,13 +53,26 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  /*  const setDays = days => setState(prev => ({ ...prev, days })); */
 
   useEffect(() => {
-    axios.get('/api/days').then(resp => setDays([...resp.data]));
+
+    Promise.all([
+      axios.get('/api/days'), //days
+      axios.get('/api/appointments'), //appointments
+      axios.get('/api/interviewers') //interviews
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      const [first, second, third] = all;
+
+      console.log(first, second, third);
+    });
+
+
   }, [])
 
   const dailyAppointments = [];
